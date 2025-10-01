f2fe4c006e4370fd8a907a5674989285a1ee640e
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-10-02 00:00:56 +0300

    test case added

--- twoSum.java
+++ twoSum.java
@@ -2,13 +2,13 @@
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
         for (int i=0; i<nums.length; i++){
-            for (int j=0; j<nums.length; j++){
+            for (int j=i+1; j<nums.length; j++){
                 int sum = nums[i] + nums[j];
                 if (sum == target){
                     
                     arr[0] = i;
                     arr[1] = j;
-                    break;
+                    return new int[] { i, j }; 
                     
                 }else{
                     continue;
@@ -16,8 +16,23 @@
             }
             
         }
-        return arr;
+        throw new IllegalArgumentException("no solution");
     }
 
+}
+
+public class twoSum {
+    public static void main(String[] args) {
+int[] nums = {2, 4, 11, 3};
+        int target = 6;
+        
+        Solution solution = new Solution(); 
+
+        int[] result = solution.twoSum(nums, target); 
+        
+        
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+    
 }
 
